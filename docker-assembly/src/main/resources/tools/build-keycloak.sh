ef5050c7a47a0208b7ad63b8aadbdbafd96abd1c
Ed Legaspi <czetsuya@gmail.com>
2022-10-05 12:52:54 +0800

    others: keycloak successfully running on version 16.1.1

--- docker-assembly/src/main/resources/tools/build-keycloak.sh
+++ docker-assembly/src/main/resources/tools/build-keycloak.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-###########################
-# Download and Build Keycloak
-###########################
+###############################
+# Download and Build Keycloak #
+###############################
 
 cd /opt/jboss/
 
 keycloakDistLocalFile="/opt/jboss/keycloak_install_stage/downloads/$KEYCLOAK_DIST_FILE"
 keycloakDistUrl="$KEYCLOAK_DIST_BASE$KEYCLOAK_DIST_FILE"
+
 if [ -e $keycloakDistLocalFile ];
 then
     echo "Keycloak from downloads folder: $keycloakDistLocalFile"
@@ -19,78 +20,34 @@ fi
 
 mv /opt/jboss/keycloak-??.?.?* /opt/jboss/keycloak
 
-#####################
-# Create DB modules #
-#####################
+###########################
+# Create Database Modules #
+###########################
 
-#mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
-#cd /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
-#
-#mysqlConnectorLocalFile="/opt/jboss/keycloak_install_stage/downloads/$JDBC_MYSQL_JAR_FILE"
-#mysqlConnectorUrl="$JDBC_MYSQL_JAR_BASE$JDBC_MYSQL_JAR_FILE"
-#if [ -e $mysqlConnectorLocalFile ];
-#then
-#    echo "MySql connector from [local download]: $mysqlConnectorLocalFile"
-#    cp $mysqlConnectorLocalFile .
-#else
-#    echo "MySql connector from [download]: $mysqlConnectorUrl"
-#    curl -O "$mysqlConnectorUrl"
-#fi
-#
-#cp /opt/jboss/tools/databases/mysql/module.xml .
+mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
+cd /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
 
-######################
-# Configure Keycloak #
-######################
+mysqlConnectorLocalFile="/opt/jboss/keycloak_install_stage/downloads/$JDBC_MYSQL_JAR_FILE"
+mysqlConnectorUrl="$JDBC_MYSQL_JAR_BASE$JDBC_MYSQL_JAR_FILE"
+if [ -e $mysqlConnectorLocalFile ];
+then
+    echo "MySql connector from downloads folder: $mysqlConnectorLocalFile"
+    cp $mysqlConnectorLocalFile .
+else
+    echo "MySql connector from remote source: $mysqlConnectorUrl"
+    curl -O "$mysqlConnectorUrl"
+fi
 
-#cd /opt/jboss/keycloak
-#
-#bin/jboss-cli.sh --file=/opt/jboss/tools/cli/standalone-configuration.cli
-#rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
-#
-#bin/jboss-cli.sh --file=/opt/jboss/tools/cli/standalone-ha-configuration.cli
-#rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
-
-########################
-# Install CX providers #
-########################
-
-#ls -l /opt/jboss/keycloak_install_stage/keycloak_providers
-#
-#mkdir -p /opt/jboss/keycloak/providers
-#cp /opt/jboss/keycloak_install_stage/keycloak_providers/*.jar /opt/jboss/keycloak/providers
-#
-#ls -laR /opt/jboss/keycloak/providers
-
-##########################
-# Copy CX template files #
-##########################
-
-#ls -laR /opt/jboss/keycloak_install_stage/themes
-#
-#mkdir -p /opt/jboss/keycloak/themes
-#cp -R /opt/jboss/keycloak_install_stage/themes/* /opt/jboss/keycloak/themes
-#
-#ls -laR /opt/jboss/keycloak/themes
-
-##########################
-# Copy override files    #
-##########################
-
-#ls -laR /opt/jboss/keycloak/modules/system/layers/keycloak/org/keycloak/keycloak-services/main
-#
-#cp -Rf /opt/jboss/keycloak_install_stage/override/keycloak-services-8.0.2.jar /opt/jboss/keycloak/modules/system/layers/keycloak/org/keycloak/keycloak-services/main/keycloak-services-8.0.2.jar
-#
-#ls -laR /opt/jboss/keycloak/modules/system/layers/keycloak/org/keycloak/keycloak-services/main
+cp /opt/jboss/tools/databases/mysql/module.xml .
 
 ###################
-# Set permissions #
+# Set Permissions #
 ###################
 
 echo "jboss:x:1000:jboss" >> /etc/group
 echo "jboss:x:1000:1000:JBoss user:/opt/jboss:/sbin/nologin" >> /etc/passwd
 chown -R jboss:jboss /opt/jboss
 chmod -R g+rw /opt/jboss
-ls /opt/jboss/tools
+
 rm -rf /opt/jboss/keycloak/standalone/tmp/auth
 rm -rf /opt/jboss/keycloak/domain/tmp/auth
--- docker-assembly/src/main/resources/tools/docker-entrypoint.sh
+++ docker-assembly/src/main/resources/tools/docker-entrypoint.sh
@@ -6,115 +6,53 @@ set -eou pipefail
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-    local var="$1"
-    local fileVar="${var}_FILE"
-    local def="${2:-}"
-    if [[ ${!var:-} && ${!fileVar:-} ]]; then
-        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-        exit 1
-    fi
-    local val="$def"
-    if [[ ${!var:-} ]]; then
-        val="${!var}"
-    elif [[ ${!fileVar:-} ]]; then
-        val="$(< "${!fileVar}")"
-    fi
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
 
-    if [[ -n $val ]]; then
-        export "$var"="$val"
-    fi
+  if [[ ${!var:-} && ${!fileVar:-} ]]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
 
-    unset "$fileVar"
+  local val="$def"
+  if [[ ${!var:-} ]]; then
+    val="${!var}"
+  elif [[ ${!fileVar:-} ]]; then
+    val="$(<"${!fileVar}")"
+  fi
+
+  if [[ -n $val ]]; then
+    export "$var"="$val"
+  fi
+
+  unset "$fileVar"
 }
 
 SYS_PROPS=""
 
-##################
-# Add admin user #
-##################
+#####################
+# Create Admin User #
+#####################
 
 file_env 'KEYCLOAK_USER'
 file_env 'KEYCLOAK_PASSWORD'
 
 if [[ -n ${KEYCLOAK_USER:-} && -n ${KEYCLOAK_PASSWORD:-} ]]; then
-    /opt/jboss/keycloak/bin/add-user-keycloak.sh --user "$KEYCLOAK_USER" --password "$KEYCLOAK_PASSWORD"
+  /opt/jboss/keycloak/bin/add-user-keycloak.sh --user "$KEYCLOAK_USER" --password "$KEYCLOAK_PASSWORD"
 fi
 
 #########
-# DEBUG #
+# Debug #
 #########
 
 if [[ -n ${DEBUG_PORT:-} ]]; then
-    SYS_PROPS+=" --debug $DEBUG_PORT"
+  SYS_PROPS+=" --debug $DEBUG_PORT"
 fi
 
-############
-# Hostname #
-############
-
-if [[ -n ${KEYCLOAK_FRONTEND_URL:-} ]]; then
-    SYS_PROPS+=" -Dkeycloak.frontendUrl=$KEYCLOAK_FRONTEND_URL"
-fi
-
-if [[ -n ${KEYCLOAK_HOSTNAME:-} ]]; then
-    SYS_PROPS+="-Dkeycloak.hostname.provider=fixed -Dkeycloak.hostname.fixed.hostname=$KEYCLOAK_HOSTNAME"
-
-    if [[ -n ${KEYCLOAK_HTTP_PORT:-} ]]; then
-        SYS_PROPS+=" -Dkeycloak.hostname.fixed.httpPort=$KEYCLOAK_HTTP_PORT"
-    fi
-
-    if [[ -n ${KEYCLOAK_HTTPS_PORT:-} ]]; then
-        SYS_PROPS+=" -Dkeycloak.hostname.fixed.httpsPort=$KEYCLOAK_HTTPS_PORT"
-    fi
-
-    if [[ -n ${KEYCLOAK_ALWAYS_HTTPS:-} ]]; then
-            SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
-    fi
-fi
-
-################
-# Realm import #
-################
-
-if [[ -n ${KEYCLOAK_IMPORT:-} ]]; then
-    SYS_PROPS+=" -Dkeycloak.import=$KEYCLOAK_IMPORT"
-fi
-
-########################
-# JGroups bind options #
-########################
-
-if [[ -z ${BIND:-} ]]; then
-    BIND=$(hostname --all-ip-addresses)
-fi
-if [[ -z ${BIND_OPTS:-} ]]; then
-    for BIND_IP in $BIND
-    do
-        BIND_OPTS+=" -Djboss.bind.address=$BIND_IP -Djboss.bind.address.private=$BIND_IP "
-    done
-fi
-SYS_PROPS+=" $BIND_OPTS"
-
-#########################################
-# Expose management console for metrics #
-#########################################
-
-if [[ -n ${KEYCLOAK_STATISTICS:-} ]] ; then
-    SYS_PROPS+=" -Djboss.bind.address.management=0.0.0.0"
-fi
-
-#################
-# Configuration #
-#################
-
-# If the server configuration parameter is not present, append the HA profile.
-if echo "$@" | grep -E -v -- '-c |-c=|--server-config |--server-config='; then
-    SYS_PROPS+=" -c=standalone-ha.xml"
-fi
-
-############
-# DB setup #
-############
+##########################
+# Database Configuration #
+##########################
 
 file_env 'DB_USER'
 file_env 'DB_PASSWORD'
@@ -122,7 +60,7 @@ file_env 'DB_PASSWORD'
 export DB_VENDOR="mysql"
 DB_NAME="MySQL"
 
-# Append '?' in the beginning of the string if JDBC_PARAMS value isn't empty
+# Append '?' in the beggining of the string if JDBC_PARAMS value isn't empty
 JDBC_PARAMS=$(echo "${JDBC_PARAMS:-}" | sed '/^$/! s/^/?/')
 export JDBC_PARAMS
 
@@ -137,30 +75,17 @@ function set_legacy_vars() {
     fi
   done
 }
+
 set_legacy_vars "$(echo "$DB_VENDOR" | tr "[:upper:]" "[:lower:]")"
 
-# Configure DB
-
 echo "========================================================================="
 echo ""
-echo "  Using $DB_NAME database"
+echo "  Using $DB_NAME database with vendor $DB_VENDOR"
 echo ""
 echo "========================================================================="
 echo ""
-
 
 /bin/sh /opt/jboss/tools/databases/change-database.sh $DB_VENDOR
-
-#/opt/jboss/tools/x509.sh
-#/opt/jboss/tools/jgroups.sh
-#/opt/jboss/tools/infinispan.sh
-#/opt/jboss/tools/statistics.sh
-#/opt/jboss/tools/autorun.sh
-#/opt/jboss/tools/vault.sh
-
-##################
-# Start Keycloak #
-##################
 
 exec /opt/jboss/keycloak/bin/standalone.sh $SYS_PROPS $@
 exit $?
